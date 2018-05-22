449fa0efa8d7447ec880a1ee88134bb8c09cacd7
Wang Xu <121894598@qq.com>
2018-05-22 17:17:41 +0800

    Initial Commit

--- src/main/java/xyz/amazingxu/wxblog/domain/UserDO.java
+++ src/main/java/xyz/amazingxu/wxblog/domain/UserDO.java
@@ -12,10 +12,43 @@ import javax.persistence.Entity;
 @Entity(name = "userinfo")
 public class UserDO extends BaseDO{
 
+    @Override
+    public String toString() {
+        return "UserDO{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", user_id='" + user_id + '\'' +
+                '}';
+    }
+
     @Column(name = "user_id",length = 40)
     private String user_id;
-
     @Column
     private String username;
+    @Column(name = "password",nullable = false , length = 20)
+    private String password;
 
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
