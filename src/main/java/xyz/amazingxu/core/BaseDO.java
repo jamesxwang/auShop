24f51decd6875d865563940407902b357af33fdf
Wang Xu <121894598@qq.com>
2018-05-22 16:32:17 +0800

    Initial Commit

--- src/main/java/xyz/amazingxu/core/BaseDO.java
+++ src/main/java/xyz/amazingxu/core/BaseDO.java
@@ -1,6 +1,7 @@
 package xyz.amazingxu.core;
 
 import javax.persistence.*;
+import java.util.Date;
 
 /**
  * 领域基类
@@ -23,6 +24,61 @@ public abstract class BaseDO {
      */
     @Column(name = "is_deleted",nullable = false)
     private boolean deleted;
+    /**
+     * 创建人
+     */
+    @Column(name = "create_user", nullable = false, length = 40)
+    private String createUser;
 
+    /**
+     * 创建时间
+     */
+    @Column(name = "create_time", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+    /**
+     * 备注
+     */
+    @Column(name = "remark", length = 100)
+    private String remark;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
 }
--- src/main/java/xyz/amazingxu/wxblog/domain/UserDO.java
+++ src/main/java/xyz/amazingxu/wxblog/domain/UserDO.java
@@ -1,4 +1,21 @@
 package xyz.amazingxu.wxblog.domain;
 
-public class UserDO {
+import xyz.amazingxu.core.BaseDO;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+/**
+ * @author xuwang <121894598@qq.com>
+ * @date 2018/5/22 15:33
+ */
+@Entity(name = "userinfo")
+public class UserDO extends BaseDO{
+
+    @Column(name = "user_id",length = 40)
+    private String user_id;
+
+    @Column
+    private String username;
+
 }
