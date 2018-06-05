f2420787b0d69be2cf06f6064a3344d8274f3fb2
Wang Xu <121894598@qq.com>
2018-06-05 11:16:14 +0800

    update register restrictions with regex

--- src/main/java/xyz/amazingxu/wxblog/service/impl/UserServiceImpl.java
+++ src/main/java/xyz/amazingxu/wxblog/service/impl/UserServiceImpl.java
@@ -88,14 +88,31 @@ public class UserServiceImpl extends BaseServiceImpl implements IUserService {
 
     @Override
     public void register(UserRegisterReqDTO userRegisterReqDTO) {
-        //TODO 这里可以用正则表达式优化
+        //TODO 细化报错信息
+        //账号只能是字母开头，允许6-20字节，允许字母数字下划线
+        String usernamePattern = "^[a-zA-Z][a-zA-Z0-9_]{6,20}$";
+//        密码只能是0~9, a~z, A~Z,特殊字符包括  ^ % & ' , ; = ? $   长度为6~20
+        String passwordPattern = "[\\u4E00-\\u9FA5A-Za-z0-9_^%&',;=?$]{6,20}";
+        //标准邮箱格式
+        String emailPattern = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
+
         if (userRegisterReqDTO.getUsername().equals("") || userRegisterReqDTO.getName().equals("") || userRegisterReqDTO.getPassword().equals("")) {
-            throw new wxblogException("Username, Full name or Password cannot be empty!");
-        } else if (userRegisterReqDTO.getPassword().contains("'")) {
-            throw new wxblogException("Password invalid!");
-        } else if (userRegisterReqDTO.getPassword().length()<6){
-            throw new wxblogException("Password must be at least 6 words!");
-        } else if (!userRegisterReqDTO.getEmail().contains("@") && !userRegisterReqDTO.getEmail().contains(".")){
+            throw new wxblogException("Username, Full name or Password is empty!");
+        } else if (!userRegisterReqDTO.getUsername().matches(usernamePattern)){
+            if (userRegisterReqDTO.getUsername().length()<6 || userRegisterReqDTO.getUsername().length()>20){
+                throw new wxblogException("Username must be 6~20 characters!");
+            }else {
+                throw new wxblogException("Username must begin with letters!");
+            }
+        } else if (userRegisterReqDTO.getName().length()>40){
+                throw new wxblogException("Full Name must be 0~40 characters!");
+        } else if (!userRegisterReqDTO.getPassword().matches(passwordPattern)) {
+            if (userRegisterReqDTO.getPassword().length()<6 || userRegisterReqDTO.getPassword().length()>20){
+                throw new wxblogException("Password must be 6~20 characters!");
+            }else {
+                throw new wxblogException("Password can only contain letters, numbers and ^%&',;=?$ !");
+            }
+        } else if (!userRegisterReqDTO.getEmail().matches(emailPattern)){
            throw new wxblogException("Incorrect e-mail format!");
         } else {
             //查找账号是否存在
