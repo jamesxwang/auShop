da758cdae8924187986bd10673832bdda9372e0f
Wang Xu <121894598@qq.com>
2018-06-07 10:33:43 +0800

    update

--- src/main/resources/static/web/js/component.js
+++ src/main/resources/static/web/js/component.js
@@ -24,28 +24,28 @@ avalon.component('ms-nav1',{
     '                        <div class="form-group" >\n' +
     '                            <input type="text" class="form-control" placeholder="Search" autocomplete="off">\n' +
     '                        </div>\n' +
-    '                        <button type="submit" class="btn btn-default m-l-sm"><a>Search</a></button>\n' +
+    '                        <button type="submit" class="btn btn-default m-l-sm"><a class="navbarFont">Search</a></button>\n' +
     '                        <div class="search-result hidden"></div>\n' +
     '                    </form></li>\n' +
     '\n' +
     '                    <!--Register-->\n' +
-    '                    <li><a href="web/register.html" ms-if="showLoginStatus()"><p>Sign up</p></a></li>\n' +
+    '                    <li><a href="web/register.html" ms-if="showLoginStatus()" class="navbarFont"><p>Sign up</p></a></li>\n' +
     '                    <!--Login-->\n' +
-    '                    <li><a href="web/login.html" ms-if="showLoginStatus()"><p>Sign in</p></a></li>\n' +
+    '                    <li><a href="web/login.html" ms-if="showLoginStatus()" class="navbarFont"><p>Sign in</p></a></li>\n' +
     '                    <!--Welcome-->\n' +
-    '                    <li><a href="web/user_info.html" ms-if="!showLoginStatus()"><p>Welcome, {{UserInfo.name}}</p></a></li>\n' +
+    '                    <li><a href="web/userInfo.html" ms-if="!showLoginStatus()" class="navbarFont"><p>Welcome, {{UserInfo.name}}</p></a></li>\n' +
     '\n' +
     '                    <!--Settings-->\n' +
     '                    <li class="dropdown">\n' +
-    '                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"\n' +
+    '                        <a class="dropdown-toggle navbarFont" data-toggle="dropdown" role="button" aria-haspopup="true"\n' +
     '                           aria-expanded="false" ms-if="!showLoginStatus()">Settings <span class="caret"></span></a>\n' +
     '                        <ul class="dropdown-menu">\n' +
-    '                            <li><a href="#">Account</a></li>\n' +
-    '                            <li><a href="#">Safe settings</a></li>\n' +
-    '                            <li><a href="#">Messages</a></li>\n' +
+    '                            <li><a href="#" class="navbarFont">Account</a></li>\n' +
+    '                            <li><a href="#" class="navbarFont">Safe settings</a></li>\n' +
+    '                            <li><a href="#" class="navbarFont">Messages</a></li>\n' +
     '                            <li role="separator" class="divider"></li>\n' +
     '                            <!--<li class="dropdown-header">Nav header</li>-->\n' +
-    '                            <li><a href="#" ms-on-click="DoLogOut()">Log out</a></li>\n' +
+    '                            <li><a href="#" ms-on-click="DoLogOut()" class="navbarFont">Log out</a></li>\n' +
     '                            <!--<li><a href="#">One more separated link</a></li>-->\n' +
     '                        </ul>\n' +
     '                    </li>\n' +
@@ -94,28 +94,28 @@ avalon.component('ms-nav',{
     '                        <div class="form-group" >\n' +
     '                            <input type="text" class="form-control" placeholder="Search" autocomplete="off">\n' +
     '                        </div>\n' +
-    '                        <button type="submit" class="btn btn-default m-l-sm"><a>Search</a></button>\n' +
+    '                        <button type="submit" class="btn btn-default m-l-sm"><a class="navbarFont">Search</a></button>\n' +
     '                        <div class="search-result hidden"></div>\n' +
     '                    </form></li>\n' +
     '\n' +
     '                    <!--Register-->\n' +
-    '                    <li><a href="register.html" ms-if="showLoginStatus()"><p>Sign up</p></a></li>\n' +
+    '                    <li><a href="register.html" ms-if="showLoginStatus()" class="navbarFont"><p>Sign up</p></a></li>\n' +
     '                    <!--Login-->\n' +
-    '                    <li><a href="login.html" ms-if="showLoginStatus()"><p>Sign in</p></a></li>\n' +
+    '                    <li><a href="login.html" ms-if="showLoginStatus()" class="navbarFont"><p>Sign in</p></a></li>\n' +
     '                    <!--Welcome-->\n' +
-    '                    <li><a href="user_info.html" ms-if="!showLoginStatus()"><p>Welcome, {{UserInfo.name}}</p></a></li>\n' +
+    '                    <li><a href="userInfo.html" ms-if="!showLoginStatus()" class="navbarFont"><p>Welcome, {{UserInfo.name}}</p></a></li>\n' +
     '\n' +
     '                    <!--Settings-->\n' +
     '                    <li class="dropdown">\n' +
-    '                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"\n' +
+    '                        <a class="dropdown-toggle navbarFont" data-toggle="dropdown" role="button" aria-haspopup="true"\n' +
     '                           aria-expanded="false" ms-if="!showLoginStatus()">Settings <span class="caret"></span></a>\n' +
     '                        <ul class="dropdown-menu">\n' +
-    '                            <li><a href="#">My Account</a></li>\n' +
-    '                            <li><a href="#">Safe settings</a></li>\n' +
-    '                            <li><a href="#">Messages</a></li>\n' +
+    '                            <li><a href="#" class="navbarFont">My Account</a></li>\n' +
+    '                            <li><a href="#" class="navbarFont">Safe settings</a></li>\n' +
+    '                            <li><a href="#" class="navbarFont">Messages</a></li>\n' +
     '                            <li role="separator" class="divider"></li>\n' +
     '                            <!--<li class="dropdown-header">Nav header</li>-->\n' +
-    '                            <li><a href="#" ms-on-click="DoLogOut()">Log out</a></li>\n' +
+    '                            <li><a href="#" ms-on-click="DoLogOut()" class="navbarFont">Log out</a></li>\n' +
     '                            <!--<li><a href="#">One more separated link</a></li>-->\n' +
     '                        </ul>\n' +
     '                    </li>\n' +
