29a2ff864a0a507a7b6c06add18febd1c58d4445
Wang Xu <36892657+CN-WX@users.noreply.github.com>
2018-07-10 19:22:36 +1000

    Update WxblogApplication.java

--- src/main/java/xyz/amazingxu/WxblogApplication.java
+++ src/main/java/xyz/amazingxu/WxblogApplication.java
@@ -1,11 +1,16 @@
 package xyz.amazingxu;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -52,14 +57,31 @@ public class WxblogApplication {
 	}
 
 	@Bean
+	public EmbeddedServletContainerFactory servletContainer(){
+		TomcatEmbeddedServletContainerFactory tomcat=new TomcatEmbeddedServletContainerFactory(){
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint=new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");//confidential
+				SecurityCollection collection=new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(httpConnector());
+		return tomcat;
+	}
+
+	@Bean
 	public Connector httpConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setScheme("http");
 		// Connector监听的http的端口号
-		connector.setPort(8080);
-		connector.setSecure(false);
+		connector.setPort(80);
+		//connector.setSecure(false);
 		// 监听到http的端口号后转向到的https的端口号
-		connector.setRedirectPort(8443);
+		connector.setRedirectPort(443);
 		return connector;
 	}
 
