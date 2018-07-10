34b78025ec538636353df59eb8ab1e8819d4a2c0
Wang Xu <36892657+CN-WX@users.noreply.github.com>
2018-07-10 18:10:42 +1000

    Update WxblogApplication.java

--- src/main/java/xyz/amazingxu/WxblogApplication.java
+++ src/main/java/xyz/amazingxu/WxblogApplication.java
@@ -56,10 +56,10 @@ public class WxblogApplication {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setScheme("http");
 		// Connector监听的http的端口号
-		connector.setPort(8082);
+		connector.setPort(80);
 		connector.setSecure(false);
 		// 监听到http的端口号后转向到的https的端口号
-		connector.setRedirectPort(8443);
+		connector.setRedirectPort(443);
 		return connector;
 	}
 
